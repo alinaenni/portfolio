c16a5674dafbb015e3afa41f43eaf57633100966
Alina Rouvinen <98087771+Alina-enni@users.noreply.github.com>
2024-02-16 16:57:21 +0200

    Fixing the navigation links
    Navigation is functional at this point. Probably going to set a goal to update it to something more efficient later on

--- blog/components/navigation.js
+++ blog/components/navigation.js
@@ -5,9 +5,9 @@ export default function Navigation() {
     return (
 
         <div className="navigation">
-        <div><Link href="/index"><div className="normal">Home</div></Link></div>
-        <div><Link href="/blog"><div className="normal">Projects</div></Link></div>
-        <div><Link href="/contact"><div className="normal">Socials</div></Link></div>
+        <div><Link href="/"><div>Home</div></Link></div>
+        <div><Link href="/posts/projects"><div>Projects</div></Link></div>
+        <div><Link href="/posts/socials"><div>Socials</div></Link></div>
       </div>
 
     );
